6e144a4d8b1704818c589b4a25c7bf76f2da77fc
Caleb Alfaro Moreira <calebam2112@gmail.com>
2024-12-15 23:46:39 -0600

    Basic search catalog

---
+++ tmdb-app/src/components/catalog/Catalog.jsx
@@ -0,0 +1,73 @@
+import React, { useState, useEffect } from "react";
+import { tmdb, searchMovies } from "../../tmdb/config";
+import SearchArea from "./SearchArea";
+
+const Catalog = () => {
+  const [query, setQuery] = useState("");
+  const [movies, setMovies] = useState([]);
+  const [error, setError] = useState(null);
+
+//   useEffect(() => {
+//     const fetchMovies = async () => {
+//       try {
+//         const response = await tmdb.get("/movie/popular");
+//         setMovies(response.data.results);
+//       } catch (error) {
+//         console.error("Error fetching movies:", error);
+//       }
+//     };
+//     fetchMovies();
+//   }, []);
+
+  const handleSearch = async (e) => {
+    e.preventDefault();
+
+    if (!query.trim()) return;
+    console.log("noooo");
+    try {
+      const results = await searchMovies(query); 
+      setMovies(results);
+      console.log(results);
+      setError(null);
+    } catch (err) {
+      setError("Failed to fetch movies. Please try again later.");
+    }
+  };
+
+  const handleChange = (e) => {setQuery(e.target.value)};
+
+  return (
+    <div>
+       <SearchArea handleChange={handleChange} handleSubmit={handleSearch}  />
+      {/*<h1>Popular Movies</h1>
+      <ul>
+        {movies.map((movie) => (
+          <li key={movie.id}>
+            {movie.title} ({movie.release_date})
+          </li>
+        ))}
+      </ul> */}
+      
+
+      <div>
+        {movies.length > 0 ? (
+          <ul>
+            {movies.map((movie) => (
+              <li key={movie.id}>
+                <h3>{movie.title}</h3>
+                <p>Release Date: {movie.release_date || "N/A"}</p>
+                <p>{movie.overview || "No description available."}</p>
+              </li>
+            ))}
+          </ul>
+        ) : (
+          <p>No movies found. Try searching for something else!</p>
+        )}
+      </div>
+
+
+    </div>
+  );
+};
+
+export default Catalog;
