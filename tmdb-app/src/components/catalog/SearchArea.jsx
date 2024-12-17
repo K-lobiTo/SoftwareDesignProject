be3f0857aa61da39aa4a4ffa04317f69f5134abe
Caleb Alfaro Moreira <calebam2112@gmail.com>
2024-12-16 19:47:54 -0600

    UI minor changes

--- tmdb-app/src/components/catalog/SearchArea.jsx
+++ tmdb-app/src/components/catalog/SearchArea.jsx
@@ -1,21 +1,21 @@
-
-
 const SearchArea = (props) => {
-    return(
-        <div className="container">
-            <div className="row">
-                <section className="col s4 offset-4">
-                    <form action="" onSubmit={props.handleSubmit}>
-                        <div className="input field">
-                            <input placeholder="search" type="text" onChange={props.handleChange}>
-                            </input>
-                        </div>
-                    </form>
-                </section>
-
+  return (
+    <div className="containter">
+      <div className="row">
+        <section className="col s4 offset-2">
+          <form action="" onSubmit={props.handleSubmit}>
+            <div className="input field">
+              <input
+                placeholder="search"
+                type="text"
+                onChange={props.handleChange}
+              ></input>
             </div>
-        </div>
-    )
-}
+          </form>
+        </section>
+      </div>
+    </div>
+  );
+};
 
 export default SearchArea;
--- tmdb-app/src/components/header/index.jsx
+++ tmdb-app/src/components/header/index.jsx
@@ -1,29 +1,87 @@
-
-import React from 'react'
-import { Link, useNavigate } from 'react-router-dom'
-import { useAuth } from '../../contexts/authContext'
-import { doSignOut } from '../../firebase/auth'
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { useAuth } from "../../contexts/authContext";
+import { doSignOut } from "../../firebase/auth";
 
 const Header = () => {
-    const navigate = useNavigate()
-    const { userLoggedIn } = useAuth()
-    return (
-        <nav className='flex flex-row gap-x-2 w-full z-20 fixed top-0 left-0 h-12 border-b place-content-center items-center bg-gray-200'>
-            {
-                userLoggedIn
-                    ?
-                    <>
-                        <button onClick={() => { doSignOut().then(() => { navigate('/login') }) }} className='text-sm text-blue-600 underline'>Logout</button>
-                    </>
-                    :
-                    <>
-                        <Link className='text-sm text-blue-600 underline' to={'/login'}>Login</Link>
-                        <Link className='text-sm text-blue-600 underline' to={'/register'}>Register New Account</Link>
-                    </>
-            }
+  const navigate = useNavigate();
+  const { userLoggedIn, currentUser } = useAuth();
+  return (
+    <div className="container">
+      <nav>
+        <div className="nav-wrapper">
+          {/* <a href="#" class="brand-logo">
+          TheMovieDB App
+        </a> */}
+          {userLoggedIn ? (
+            <div className="right-align">
+              <button
+                onClick={() => {
+                  doSignOut().then(() => {
+                    navigate("/login");
+                  });
+                }}
+                className="waves-effect waves-light btn"
+              >
+                Logout
+              </button>
+            </div>
+          ) : (
+            <ul>
+              <li>
+                <Link className="text-sm text-blue-600 underline" to={"/login"}>
+                  Login
+                </Link>
+              </li>
+              <li>
+                <Link className="text-sm text-white-600 underline" to={"/register"}>
+                  Register
+                </Link>
+              </li>
+            </ul>
+          )}
+        </div>
+      </nav>
+    </div>
 
-        </nav>
-    )
-}
+    // <nav>
+    //   <div className="nav-wrapper">
+    //     <a href="" className="brand-logo">
+    //       Logo
+    //     </a>
+    //     <ul id="nav-mobile" className="right hide-on-med-and-down">
+    //       <li>
+    //         <a href="">sass</a>
+    //       </li>
+    //       <li>
+    //         <a href="">
+    //           sass <span className="new badge">4</span>
+    //         </a>
+    //       </li>
+    //       <li>
+    //         <a href="">sass</a>
+    //       </li>
+    //     </ul>
+    //   </div>
+    // </nav>
 
-export default Header
+    // <nav className='flex flex-row gap-x-2 w-full z-20 fixed top-0 left-0 h-12 border-b place-content-center items-center bg-gray-200'>
+
+    //     {
+    //         userLoggedIn
+    //             ?
+    //             <>
+    //                 <button onClick={() => { doSignOut().then(() => { navigate('/login') }) }} className='text-sm text-blue-600 underline'>Logout</button>
+    //             </>
+    //             :
+    //             <>
+    //                 <Link className='text-sm text-blue-600 underline' to={'/login'}>Login</Link>
+    //                 <Link className='text-sm text-blue-600 underline' to={'/register'}>Register New Account</Link>
+    //             </>
+    //     }
+
+    // </nav>
+  );
+};
+
+export default Header;
