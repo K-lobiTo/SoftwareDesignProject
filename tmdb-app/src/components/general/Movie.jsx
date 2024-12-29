97b66683ed09af45347a2ddf7d920f63a1f50318
Alevdelgado <110568267+Alevdelgado@users.noreply.github.com>
2024-12-29 00:27:33 -0600

    Movie.jsx resolucion images

--- tmdb-app/src/components/general/Movie.jsx
+++ tmdb-app/src/components/general/Movie.jsx
@@ -1,23 +1,39 @@
+// const Movie = (props) => {
+//     return(
+//         <div className="col s12 m6 l3">
+//             <div className="card">
+//                 <div className="card-image waves-effect waves-block waves-light" style={{height: "400px"}}>
+//                     {
+//                         props.image == null ? 
+//                         <img src="https://theradar.ng/_next/image?url=%2Fapi%2Fimages%2Fchillguy-1-gID_7-1732815432740-755124263.png&w=1920&q=75" alt="card image" style={{width: "100%", height: "100%", objectFit: "cover"}}></img>
+//                         :
+//                         <img src={`http://image.tmdb.org/t/p/w500${props.image}`} alt="card image" style={{width: "100%", height: "100%", objectFit: "cover"}}></img>
+//                     }
+//                 </div>
+//             </div>
+//         </div>
+//     )
+// }
+
+// export default Movie;
+
+import React from 'react';
 
 const Movie = (props) => {
-    return(
-        <div className="col s12 m6 l3">
-            <div className="card">
-                <div className="card-image waves-effect waves-block waves-light">
-                    {//we probably need to change this later to make it responsive (the height)
-                        props.image == null ? 
-                        <img src="https://theradar.ng/_next/image?url=%2Fapi%2Fimages%2Fchillguy-1-gID_7-1732815432740-755124263.png&w=1920&q=75" alt="card image" style={{width: "100%", height: 360}}></img>
-                        :
-                        <img src={`http://image.tmdb.org/t/p/w185${props.image}`} alt="card image" style={{width: "100%", height: 360}}></img>
-                        //we should consider making this a component later
-                    }
-                    <img src="">
-                    </img>
-                </div>
-            </div>
-
+  return (
+    <div className="col s12 m6 l3">
+      <div className="card">
+        <div className="card-image waves-effect waves-block waves-light" style={{ height: "400px" }}>
+          {
+            props.image == null ?
+              <img src="https://theradar.ng/_next/image?url=%2Fapi%2Fimages%2Fchillguy-1-gID_7-1732815432740-755124263.png&w=1920&q=75" alt="card image" style={{ width: "100%", height: "100%", objectFit: "cover" }}></img>
+              :
+              <img src={`http://image.tmdb.org/t/p/w500${props.image}`} alt="card image" style={{ width: "100%", height: "100%", objectFit: "cover" }}></img>
+          }
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 
 export default Movie;
