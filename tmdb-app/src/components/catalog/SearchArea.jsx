a018db7115674aaefbb08702e4d9c1c7cc940d24
Alevdelgado <110568267+Alevdelgado@users.noreply.github.com>
2024-12-29 00:44:30 -0600

    SearchArea.jsx

--- tmdb-app/src/components/catalog/SearchArea.jsx
+++ tmdb-app/src/components/catalog/SearchArea.jsx
@@ -1,19 +1,17 @@
+import React from 'react';
+
 const SearchArea = (props) => {
   return (
-    <div className="containter">
-      <div className="row">
-        <section className="col s4 offset-2">
-          <form action="" onSubmit={props.handleSubmit}>
-            <div className="input field">
-              <input
-                placeholder="search"
-                type="text"
-                onChange={props.handleChange}
-              ></input>
-            </div>
-          </form>
-        </section>
-      </div>
+    <div>
+      <form action="" onSubmit={props.handleSubmit}>
+        <div className="input-field">
+          <input
+            placeholder="search"
+            type="text"
+            onChange={props.handleChange}
+          ></input>
+        </div>
+      </form>
     </div>
   );
 };
