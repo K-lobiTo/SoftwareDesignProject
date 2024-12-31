26760ff057b3873d340159a6b24602a23acf3494
Caleb Alfaro Moreira <calebam2112@gmail.com>
2024-12-30 19:03:31 -0600

    comments removed

--- tmdb-app/src/App.jsx
+++ tmdb-app/src/App.jsx
@@ -1,6 +1,3 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 
 import { AuthProvider } from "./contexts/authContext/index";
 import { useRoutes } from "react-router-dom";
