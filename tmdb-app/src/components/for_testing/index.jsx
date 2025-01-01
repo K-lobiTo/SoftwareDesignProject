debd230c2d7ef6d24258af7fa8d5b5cff3db518c
Caleb Alfaro Moreira <calebam2112@gmail.com>
2025-01-01 16:02:30 -0600

    testing user Movies

--- tmdb-app/src/components/for_testing/index.jsx
+++ tmdb-app/src/components/for_testing/index.jsx
@@ -1,10 +1,30 @@
-import React from 'react'
-import { useAuth } from '../../contexts/authContext'
+import React, { useEffect, useState } from "react";
+import { useAuth } from "../../contexts/authContext";
+import { getMoviesByUser } from "../../firebase/firestore";
 
 const Home = () => {
-    const { currentUser } = useAuth()
-    return (
-        <div className='text-2xl font-bold pt-14'>Hello {currentUser.displayName ? currentUser.displayName : currentUser.email}, you are now logged in.</div>
-    )
-}
-export default Home
+  const { currentUser } = useAuth();
+  const [ movieList, setMovieList ] = useState([]);
+
+  useEffect(() => {
+    const fetchMovies = async () => {
+      try {
+        const usrMovies = await getMoviesByUser(currentUser);
+        setMovieList(usrMovies);
+      } catch (error) {
+        console.error("Error fetching movies:", error);
+      }
+    };
+    fetchMovies();
+  }, []);
+  return (
+    <>
+      <div className="text-2xl font-bold pt-14">
+        Hello{" "}
+        {currentUser.displayName ? currentUser.displayName : currentUser.email},
+        you are now logged in.
+      </div>
+    </>
+  );
+};
+export default Home;
