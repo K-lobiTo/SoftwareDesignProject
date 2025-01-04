200ff3a006680531b1aa1a84f952324170afe996
Caleb Alfaro Moreira <calebam2112@gmail.com>
2025-01-03 22:45:27 -0600

    register texts added in en-US and es-MX

--- tmdb-app/src/contexts/languageProvider/index.jsx
+++ tmdb-app/src/contexts/languageProvider/index.jsx
@@ -1,142 +1,157 @@
-import React, {createContext, useContext, useState} from "react";
+import React, { createContext, useContext, useState } from "react";
 
-const languageNames={
-  'en-US': {
+const languageNames = {
+  "en-US": {
+    header: {
+      catalog: "Catalog",
+      watchList: "Watch List",
+      config: "Configuration",
+      signOut: "Sign Out",
 
-    header:{
-        catalog: 'Catalog',
-        watchList: 'Watch List',
-        config: 'Configuration',
-        signOut: 'Sign Out',
-
-        // CONFIGURATION
-        language: 'Language',
-        theme: 'Theme Color: ',
-        spanish: 'Spanish',
-        english: 'English',
-        light: 'Light',
-        dark: 'Dark',
+      // CONFIGURATION
+      language: "Language",
+      theme: "Theme Color: ",
+      spanish: "Spanish",
+      english: "English",
+      light: "Light",
+      dark: "Dark",
     },
 
-    catalog:{
-      catalog: 'Catalog',
+    catalog: {
+      catalog: "Catalog",
 
       // MOVIE CARD
-      punctuation: 'Punctuation: ',
-      releaseDate: 'Release: ',
+      punctuation: "Punctuation: ",
+      releaseDate: "Release: ",
 
       // SEARCH BAR
-      search: 'Search by title',
-      genre: 'Genres',
-      allGenres: 'All Genres',
+      search: "Search by title",
+      genre: "Genres",
+      allGenres: "All Genres",
     },
 
-    details:{
+    details: {
       // Card Title
-      punctuation: 'Punctuation:',
-      overView: 'Overview:',
+      punctuation: "Punctuation:",
+      overView: "Overview:",
 
       // Details
-      cast: 'Cast',
-      images: 'Images',
-      notImages: 'No images available',
+      cast: "Cast",
+      images: "Images",
+      notImages: "No images available",
 
       // Card Info
-      originalTitle: 'Original Title:',
-      production: 'Production Company:',
-      runtime: 'Runtime:',
-      minutes: 'minutes',
-      language: 'Language:',
-      budget: 'Budget:',
-      revenue: 'Revenue:',
-      undefined: 'Unknown',
+      originalTitle: "Original Title:",
+      production: "Production Company:",
+      runtime: "Runtime:",
+      minutes: "minutes",
+      language: "Language:",
+      budget: "Budget:",
+      revenue: "Revenue:",
+      undefined: "Unknown",
     },
 
-    login:{
-      email: 'Email',
-      password: 'Password',
-      signIn: 'Sign In',
-      registerMsg: 'Are you new here? Sign Up',
-      googleAuthMsg: 'Continue with Google',
-      signingInMsg: 'Signing In...',
-    }
-
+    auth: {
+      //general
+      email: "Email",
+      password: "Password",
+      //SignIn
+      signIn: "Sign In",
+      registerMsg: "Are you new here? Sign Up",
+      googleAuthMsg: "Continue with Google",
+      signingInMsg: "Signing In...",
+      //SignUp
+      signUp: "Sign Up",
+      register: "Create Account",
+      confirmPassword: "Confirm your password",
+      signInMsg: "Already have an account?",
+    },
+    appName: "WatchList",
   },
-  'es-MX': {
-    header:{
-        catalog: 'Catálogo',
-        watchList: 'Lista de Reproducción',
-        config: 'Configuración',
-        signOut: 'Salir',
+  "es-MX": {
+    header: {
+      catalog: "Catálogo",
+      watchList: "Lista de Reproducción",
+      config: "Configuración",
+      signOut: "Salir",
 
-        // CONFIGURATION
-        language: 'Idioma',
-        theme: 'Color del Tema: ',
-        spanish: 'Español',
-        english: 'Inglés',
-        light: 'Claro',
-        dark: 'Oscuro',
+      // CONFIGURATION
+      language: "Idioma",
+      theme: "Color del Tema: ",
+      spanish: "Español",
+      english: "Inglés",
+      light: "Claro",
+      dark: "Oscuro",
     },
 
-    catalog:{
-      catalog: 'Catálogo',
+    catalog: {
+      catalog: "Catálogo",
 
       // MOVIE CARD
-      punctuation: 'Puntuación: ',
-      releaseDate: 'Estreno: ',
+      punctuation: "Puntuación: ",
+      releaseDate: "Estreno: ",
 
       // SEARCH BAR
-      search: 'Buscar por título',
-      genre: 'Géneros',
-      allGenres: 'Todos los Géneros',
+      search: "Buscar por título",
+      genre: "Géneros",
+      allGenres: "Todos los Géneros",
     },
 
-    details:{
+    details: {
       // Card Title
-      punctuation: 'Puntuación:',
-      overView: 'Resumen:',
+      punctuation: "Puntuación:",
+      overView: "Resumen:",
 
       // Details
-      cast: 'Reparto',
-      images: 'Imágenes',
-      notImages: 'No hay imágenes disponibles',
+      cast: "Reparto",
+      images: "Imágenes",
+      notImages: "No hay imágenes disponibles",
 
       // Card Info
-      originalTitle: 'Título Original:',
-      production: 'Productora:',
-      runtime: 'Duración:',
-      minutes: 'minutos',
-      language: 'Idioma:',
-      budget: 'Presupuesto:',
-      revenue: 'Ingresos:',
-      undefined: 'Desconocido',
+      originalTitle: "Título Original:",
+      production: "Productora:",
+      runtime: "Duración:",
+      minutes: "minutos",
+      language: "Idioma:",
+      budget: "Presupuesto:",
+      revenue: "Ingresos:",
+      undefined: "Desconocido",
     },
 
-    login:{
-      email: 'Correo',
-      password: 'Contraseña',
-      signIn: 'Inicia Sesión',
-      registerMsg: 'Eres nuevo aquí? Regístrate',
-      googleAuthMsg: 'Continuar con google',
-      signingInMsg: 'Iniciando sesión...',
-    }
+    auth: {
+      //general
+      email: "Correo",
+      password: "Contraseña",
+      //SignIn
+      signIn: "Inicia Sesión",
+      registerMsg: "Eres nuevo aquí? Regístrate",
+      googleAuthMsg: "Continuar con google",
+      signingInMsg: "Iniciando sesión...",
+      //SignUp
+      signUp: "Crear Cuenta",
+      register: "Registrarse",
+      confirmPassword: "Confirmar contraseña",
+      signInMsg: "Ya tienes una cuenta?",
+    },
+    appName: "WatchList",
   },
 };
-
 
 const LanguageContext = createContext();
 
 function LanguageProvider(props) {
-const [language, setLanguage] = useState('en-US');
-const toggleLanguage = () => {
-  setLanguage(language === 'en-US' ? 'es-MX' : 'en-US');
-};
+  const [language, setLanguage] = useState("en-US");
+  const toggleLanguage = () => {
+    setLanguage(language === "en-US" ? "es-MX" : "en-US");
+  };
 
-const value = { language: languageNames[language], toggleLanguage, languageName: language };
+  const value = {
+    language: languageNames[language],
+    toggleLanguage,
+    languageName: language,
+  };
 
-return (
-  <LanguageContext.Provider value={value} {...props} />
-);    
+  return <LanguageContext.Provider value={value} {...props} />;
 }
 
 const useLanguage = () => useContext(LanguageContext);
